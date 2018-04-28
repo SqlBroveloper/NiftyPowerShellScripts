f45206da2425541991ddbac7f54b52782bb8e1ae
Alex Fleming <30946691+SqlBroveloper@users.noreply.github.com>
2018-04-28 00:56:07 -0400

    Add files via upload

---
+++ Get-Scripts.ps1
@@ -0,0 +1,42 @@
+﻿function Get-Scripts(
+<####################################################################
+Author: Alex Fleming
+Created On: 04/28/2018
+Description: Returns all script files based on the directoy, and search criteria.
+Recursion is optional.  The maximum number of files returned must not exceed 50.
+####################################################################>
+    [string]$Directory, 
+    [string]$Criteria,
+    [int16]$RecursionFlag
+    )
+{
+    Set-Location $Directory;
+    #Add a switch to turn recurision on/off
+    $Result = 
+    If($RecursionFlag -eq 1)
+    {
+        Get-ChildItem -Recurse |
+        Where-Object{
+            $_.Name -like "*$Criteria*"
+        }
+    }
+    ElseIf ($RecursionFlag -eq 0)
+    {
+        Get-ChildItem |
+        Where-Object{
+            $_.Name -like "*$Criteria*" -and 
+            $_.Length -gt 1
+        }
+    }
+    #CountCheck
+    If($Result.Count -le 50)
+    {
+    Return $Result;
+    }
+    Else
+    {
+        Write-Host("The number of scripts found exceeded 50; 
+            please refine your search criteria and try again.");
+    }
+}
+
