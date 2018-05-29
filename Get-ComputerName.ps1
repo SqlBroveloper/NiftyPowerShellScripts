a0cc2be8b5b16c0ee4e61dada32059d53625bb74
Alex Fleming <30946691+SqlBroveloper@users.noreply.github.com>
2018-05-29 01:30:03 -0400

    Get-ComputerName Function
    This is my first attempt at a function that returns the local computer name.

---
+++ Get-ComputerName.ps1
@@ -0,0 +1,20 @@
+﻿function Get-ComputerName
+{
+<################################################################################
+Returns the computer name based on the UserDomain value in the Environment drive
+################################################################################>
+    [string]$EnvName = (Get-PSDrive | 
+    Where-Object{$_.Name -Like "*Env*"});
+    $EnvName += ":";
+
+    If(Test-Path $EnvName)
+    {
+        Set-Location $EnvName;
+        Return (Get-ChildItem |
+        Where-Object{$_.Name -eq "UserDomain"}).Value;
+    }
+    Else
+    {
+        Write-Host "The environment/drive name is invalid."
+    }
+}
